816d41153beaf32bde4895ababe7201af7bd9238
mardonbek <mardonbek.00.tursunaliyev>
2023-08-23 12:04:30 +0500

    if else & switch case

---
+++ app.js
@@ -0,0 +1,85 @@
+// [prava va pasport olish qoidalariga misol]
+
+// let age = 10;
+// if(age >= 16 && age >= 18){
+//     console.log("pasport va prava olish huquqiga ega");
+// }else if(age >= 16 && age < 18){
+//     console.log("pasport olisa boladi lekin prava yoshiga hali yetmagan");
+// }else {
+//     console.log("hali voyaga yetmagan");
+// }
+
+// [juf va toq sonlarni aniqlash]
+// let son = 122;
+// if(son % 2 === 0){
+//     console.log("juft");
+// }else{
+//     console.log("toq");
+// }
+
+
+// [katta kichik sonni qniqlash (if else)]
+// let a = 8, b = 8;
+// if(a > b){
+//     console.log(`a = ${a} soni b = ${b} sonidan katta`);
+// }else if(a < b){
+//     console.log(`a = ${a} b = ${b} sonidan kichik`);
+// }else{
+//     console.log("sonlar teng");
+// }
+
+
+// let day = "juma"
+// switch(day){
+// case "dushamba":console.log("dushamba");break
+// case "seshamba":console.log("seshamba");break;
+// case "charshamba":console.log("chorshamb");break;
+// case "payshamba":console.log("payshamba");break;
+// case 
+// "juma":console.log("juma muborak");break;
+// case 
+// "shamba":console.log("shamba");break;
+// case "seshamba":console.log("seshamba");break;
+
+// }
+
+// let talaba = 52;
+// if (talaba >= 90){
+//     console.log("S bilan baxolandi");
+// }else if(talaba <= 90 && talaba >= 80){
+//     console.log("A bilan baxolandi");
+// }else if(talaba <= 80 && talaba >= 70){
+//     console.log("B bilan baxolandi");
+// }else if(talaba <= 70 && talaba >= 60){
+//     console.log("C bilan baxolandi");
+// }else{
+//     console.log("imtixondan uta olmadingiz");
+// }
+
+// let son = 7;
+// if (son % 2 === 0){
+//     console.log("juft son");
+// }else{
+//     console.log("toq son");
+// }
+
+// let a = 10, b = 10;
+// if(a > b){
+//     console.log(`a = ${a} soni b = ${b} sonidan katta`);
+// }else if(a < b){
+//     console.log(`a = ${a} soni b = ${b} sonidan kichi`);
+// }else{
+//     console.log("sonlar teng");
+// }
+
+
+// let day = "dushanba"
+// switch(day){
+//    case "dushanba": console.log("IT urganishim kerak"); 
+//    case "seshanba": console.log("ingiliz tilini organishi kerak"); 
+//    case "chorshanba": console.log("yapon tilini urganishim kerak"); 
+//    case "payshanba": console.log("suzushga borishim kerak"); 
+//    case "juma": console.log("uy ishlarini qilib juma nomoziga borishim kera"); 
+//    case "shanba": console.log("dostkarim bilan futbolga borishim kerak"); 
+//    case "yakshanba": console.log("oila davrasida vaqt utkazishim kerak"); 
+// }
